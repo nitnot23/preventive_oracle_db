1ab419fd84daa1fdd642b5741ed4752a1c72059d
Yunita Darmastuti <114557576+nitnot23@users.noreply.github.com>
2024-04-12 20:26:54 +0700

    export_import_progress.sql

---
+++ export_import_progress.sql
@@ -0,0 +1,6 @@
+SELECT OPNAME, SID, SERIAL#, CONTEXT, SOFAR, TOTALWORK, ROUND(SOFAR/TOTALWORK*100,2) "%_COMPLETE"
+FROM V$SESSION_LONGOPS WHERE OPNAME in
+( select d.job_name from v$session s, v$process p, dba_datapump_sessions d
+where p.addr=s.paddr and s.saddr=d.saddr )
+AND OPNAME NOT LIKE '%aggregate%' AND 
+TOTALWORK != 0 AND SOFAR <> TOTALWORK;
