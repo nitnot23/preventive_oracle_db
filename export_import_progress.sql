f036c151a3b69f14c519606c5d26434a05b6b2e0
Yunita Darmastuti <114557576+nitnot23@users.noreply.github.com>
2024-04-12 20:40:17 +0700

    export_import_progress.sql

--- export_import_progress.sql
+++ export_import_progress.sql
@@ -4,3 +4,7 @@ FROM V$SESSION_LONGOPS WHERE OPNAME in
 where p.addr=s.paddr and s.saddr=d.saddr )
 AND OPNAME NOT LIKE '%aggregate%' AND 
 TOTALWORK != 0 AND SOFAR <> TOTALWORK;
+
+SELECT owner_name, job_name, operation, job_mode, state 
+FROM dba_datapump_jobs
+where state='EXECUTING';
