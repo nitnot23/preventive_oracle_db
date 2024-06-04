88fc1f0a07d0dba0e0d1be7c46aa2fe1353248b6
Yunita Darmastuti <114557576+nitnot23@users.noreply.github.com>
2024-06-04 18:22:19 +0700

    Delete stale_tble.sql

--- stale_tble.sql
+++
@@ -1,7 +0,0 @@
-select owner,
-sum(decode(stale_stats, 'YES', 1,0)) STALE,
-sum(decode(stale_stats, 'NO', 1,0)) NO_STALE,
-sum(decode(stale_stats, null, 1,0)) NULL_STALE
-from DBA_TAB_STATISTICS
-where object_type='TABLE'
-group by owner;
