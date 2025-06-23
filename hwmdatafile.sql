d3ebd407e1a03ff7f0ddc39ed2614dda59ac521b
Yunita Darmastuti <114557576+nitnot23@users.noreply.github.com>
2025-06-23 21:05:35 +0700

    Create hwmdatafile.sql

---
+++ hwmdatafile.sql
@@ -0,0 +1,23 @@
+How to SHRINK or Reduce the datafile size by finding the HWM (Doc ID 1600774.1)
+ 
+set verify off
+column file_name format a50 word_wrapped
+column smallest format 999,990 heading "Smallest|Size|Poss."
+column currsize format 999,990 heading "Current|Size"
+column savings  format 999,990 heading "Poss.|Savings"
+break on report
+compute sum of savings on report
+column value new_val blksize
+select value from v$parameter where name = 'db_block_size';
+/
+select file_name,
+       ceil( (nvl(hwm,1)*&&blksize)/1024/1024 ) smallest,
+       ceil( blocks*&&blksize/1024/1024) currsize,
+       ceil( blocks*&&blksize/1024/1024) -
+       ceil( (nvl(hwm,1)*&&blksize)/1024/1024 ) savings
+from dba_data_files a,
+     ( select file_id, max(block_id+blocks-1) hwm
+         from dba_extents
+        group by file_id ) b
+where a.file_id = b.file_id(+) order by savings desc
+/
